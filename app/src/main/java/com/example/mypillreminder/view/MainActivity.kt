ba9a974100a7c4619417b8707d28f3ec4e32bf15
arpi-es <54029566+arpi-es@users.noreply.github.com>
2022-02-12 14:51:42 +0330

    Add files via upload

---
+++ app/src/main/java/com/example/mypillreminder/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mypillreminder.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mypillreminder.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
